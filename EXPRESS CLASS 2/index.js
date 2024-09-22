4a270c03bb542def745ae8a6a54319df3e154f12
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-09-22 23:09:39 +0530

    completed class two

--- EXPRESS CLASS 2/index.js
+++ EXPRESS CLASS 2/index.js
@@ -6,13 +6,80 @@ const users = [
     name: "aaqib",
     kidneys: [
       {
-        heathy: true,
+        healthy: true,
+      },
+      {
+        healthy: true,
+      },
+      {
+        healthy: true,
+      },
+      {
+        healthy: true,
+      },
+      {
+        healthy: true,
+      },
+
+      {
+        healthy: false,
+      },
+
+      {
+        healthy: false,
+      },
+
+      {
+        healthy: false,
       },
     ],
   },
 ];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  let numberOfKidneys = users[0].kidneys.length;
+  let numberOfHealthyKidneys = () => {
+    return users[0].kidneys.filter((user) => {
+      return user.healthy === true;
+    });
+  };
+  let numberOfUnhealthyKidneys =
+    numberOfKidneys - numberOfHealthyKidneys().length;
 
-app.get("/", (req, res) => {});
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys: numberOfHealthyKidneys().length,
+    numberOfUnhealthyKidneys,
+  });
+});
+
+app.post("/", (req, res) => {
+  const ishealthy = req.body.ishealthy;
+  users[0].kidneys.push({ healthy: ishealthy });
+  res.status(200).send("hey");
+});
+
+app.put("/", (req, res) => {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({
+    msg: "done!",
+  });
+});
+
+app.delete("/", (req, res) => {
+  const healthyKidneys = () => {
+    return users[0].kidneys.filter((user) => {
+      return user.healthy === true;
+    });
+  };
+  users[0].kidneys = healthyKidneys();
+  res.json({
+    msg: "done",
+  });
+});
 
 app.listen(3000);
