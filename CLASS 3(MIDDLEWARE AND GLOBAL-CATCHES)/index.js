abaae68361d583d299738dd74179b434d6307628
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-09-30 13:09:39 +0530

    added zod

---
+++ CLASS 3(MIDDLEWARE AND GLOBAL-CATCHES)/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const zod = require("zod");
+const app = express();
+const schema = zod.array(zod.number());
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const kidneys = req.body.kidneys;
+  const response = schema.safeParse(kidneys);
+  res.send({
+    response,
+  });
+  //   const total = kidneys.length;
+  //   res.send("you have " + total + " kidneys");
+});
+
+app.listen(3000);
+ 
